d3ba6d8b73e2146b26e02370fe1fc6dde81fbd6d
threebarber <19513830+threebarber@users.noreply.github.com>
2022-12-11 19:49:34 -0500

    initial - info page

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = 
+    [
+        { 
+          "id": 1,
+          "name": "Arto Hellas", 
+          "number": "040-123456"
+        },
+        { 
+          "id": 2,
+          "name": "Ada Lovelace", 
+          "number": "39-44-5323523"
+        },
+        { 
+          "id": 3,
+          "name": "Dan Abramov", 
+          "number": "12-43-234345"
+        },
+        { 
+          "id": 4,
+          "name": "Mary Poppendieck", 
+          "number": "39-23-6423122"
+        }
+    ]
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+
+app.get("/info", (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people \n ${new Date().toLocaleString()}`)
+  });
+
+/*
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+*/
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
