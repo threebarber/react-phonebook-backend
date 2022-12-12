9c171cb16b54afca2cc0ad4daf845b11ee2d15cc
threebarber <19513830+threebarber@users.noreply.github.com>
2022-12-12 14:55:48 -0500

    3.1-3.5

--- index.js
+++ index.js
@@ -3,85 +3,86 @@ const app = express();
 
 app.use(express.json());
 
-let persons = 
-    [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    ]
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
 const generateId = () => {
   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
   return maxId + 1;
 };
 
-
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
-
 app.get("/info", (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people \n ${new Date().toLocaleString()}`)
-  });
+  response.send(
+    `Phonebook has info for ${
+      persons.length
+    } people \n ${new Date().toLocaleString()}`
+  );
+});
 
-/*
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
+  const person = persons.find((person) => person.id === id);
 
-  if (note) {
-    response.json(note);
+  if (person) {
+    response.json(person);
   } else {
     response.status(404).end();
   }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+  persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/persons", (request, response) => {
   const body = request.body;
+  console.log(body)
+    try {
+        
+        const person = {
+            name: body.name,
+            number: body.number,
+            id: generateId(),
+          };
+        
+          persons = persons.concat(person);
+        
+          response.json(person);
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
+    } catch (error) {
+        response.status(400).end();
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
+    }
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  
 });
-*/
 
 const PORT = 3001;
 app.listen(PORT, () => {
